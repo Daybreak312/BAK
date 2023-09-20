09d63bc0ad4177d0cdde40769119d3ca97f39e19
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-09-20 21:42:47 +0900

    ✨ :: UserProvider 추가

---
+++ src/main/kotlin/com/example/bak/domain/user/service/UserProvider.kt
@@ -0,0 +1,12 @@
+package com.example.bak.domain.user.service
+
+import com.example.bak.domain.user.UserRepository
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+
+@Component
+class UserProvider(
+    private val userRepository: UserRepository
+) {
+    fun currentUser() = userRepository.findByAccountId(SecurityContextHolder.getContext().authentication!!.name)
+}
